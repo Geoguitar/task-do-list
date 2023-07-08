44522312294ee65cd597fb226082f29c59bbf6cb
Geovane Silveira <geovanedsilveira@gmail.com>
2023-07-08 20:14:39 -0300

    Inicializado o projeto

---
+++ userDepartament/src/test/java/br/com/geodeveloper/userDepartament/UserDepartamentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.geodeveloper.userDepartament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDepartamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
