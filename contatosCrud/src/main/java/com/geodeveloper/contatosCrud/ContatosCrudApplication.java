1e4a20f3fa9d8722cfc65a69f874d198c4ac36f9
Geovane Silveira <geovanedsilveira@gmail.com>
2023-10-05 15:29:12 -0300

    Iniciado projeto e classe entity

---
+++ contatosCrud/src/main/java/com/geodeveloper/contatosCrud/ContatosCrudApplication.java
@@ -0,0 +1,13 @@
+package com.geodeveloper.contatosCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContatosCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContatosCrudApplication.class, args);
+	}
+
+}
---
+++ contatosCrud/src/test/java/com/geodeveloper/contatosCrud/ContatosCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geodeveloper.contatosCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContatosCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
